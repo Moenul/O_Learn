754a0bbbe2d6a504ce9fedbc315063e184a7992f
Moenul <moenul22@gmail.com>
2025-09-12 15:21:06 +0600

    Redux toolkit installed and working with test counter.

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,71 @@
+import { useSelector, useDispatch } from "react-redux";
+import {
+    increment,
+    decrement,
+    reset,
+    incrementByAmount,
+} from "../redux/slices/counterSlice";
+import { useState } from "react";
+
 const Home = () => {
+    const [selectedAmount, setSelectedAmount] = useState(3);
+
+    const count = useSelector((state) => state.counter.value);
+    const dispatch = useDispatch();
+
     return (
         <>
             <h1>Home</h1>
+            <div className="w-lg h-[400px] p-4 border bg-sky-50 mx-auto">
+                <div className="w-full flex gap-2 justify-center">
+                    <button
+                        onClick={() => {
+                            dispatch(increment());
+                        }}
+                    >
+                        Increment
+                    </button>
+                    <div className="w-1/2 text-center">{count}</div>
+                    <button
+                        onClick={() => {
+                            dispatch(decrement());
+                        }}
+                    >
+                        Decrement
+                    </button>
+                </div>
+                <div className="w-full flex gap-2 justify-between">
+                    <button
+                        onClick={() => {
+                            dispatch(reset());
+                        }}
+                    >
+                        Reset
+                    </button>
+                    <div className="flex">
+                        <select
+                            name="amount"
+                            value={selectedAmount}
+                            onChange={(e) =>
+                                setSelectedAmount(Number(e.target.value))
+                            }
+                        >
+                            {Array.from({ length: 6 }, (_, i) => (
+                                <option key={i} value={i}>
+                                    {i}
+                                </option>
+                            ))}
+                        </select>
+                        <button
+                            onClick={() => {
+                                dispatch(incrementByAmount(selectedAmount));
+                            }}
+                        >
+                            incrementByAmount
+                        </button>
+                    </div>
+                </div>
+            </div>
         </>
     );
 };
